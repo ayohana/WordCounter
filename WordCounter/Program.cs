53975bcec790190049f02dcd0b40c52251789e5d
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-06 14:01:44 -0800

    Refactor code to keep asking user if they want to continue using the application

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -6,14 +6,19 @@ namespace WordCounter
   public class Program
   {
     public static RepeatCounter newRepeatCounter { get; set; }
+    public static bool searchIsOver = false;
 
     public static void Main()
     {
       try
       {
         Welcome();
-        AskUser();
-        DisplayMessage();
+        while(!searchIsOver)
+        {
+          AskUser();
+          DisplayMatches();
+          AskBackToSearch();
+        }
       }
       catch (Exception ex)
       {
@@ -26,15 +31,15 @@ namespace WordCounter
 
     public static void Welcome()
     {
-      Console.WriteLine(Environment.NewLine + "  _      __            __  _____               __         ");
-      Console.WriteLine(" | | /| / /__  _______/ / / ___/__  __ _____  / /____ ____");
-      Console.WriteLine(" | |/ |/ / _ \\/ __/ _  / / /__/ _ \\/ // / _ \\/ __/ -_) __/");
-      Console.WriteLine(" |__/|__/\\___/_/  \\_,_/  \\___/\\___/\\_,_/_//_/\\__/\\__/_/   " + Environment.NewLine);
+      Console.WriteLine(Environment.NewLine + "   _      __            __  _____               __         ");
+      Console.WriteLine("  | | /| / /__  _______/ / / ___/__  __ _____  / /____ ____");
+      Console.WriteLine("  | |/ |/ / _ \\/ __/ _  / / /__/ _ \\/ // / _ \\/ __/ -_) __/");
+      Console.WriteLine("  |__/|__/\\___/_/  \\_,_/  \\___/\\___/\\_,_/_//_/\\__/\\__/_/   " + Environment.NewLine);
     }
 
     public static void AskUser()
     {
-      Console.WriteLine(">>> Enter a word you'd like to search for:");
+      Console.WriteLine(">>> Enter a word to search for:");
       string searchFor = Console.ReadLine();
       newRepeatCounter = new RepeatCounter(searchFor);
       if (newRepeatCounter.IsInvalidInput())
@@ -50,19 +55,39 @@ namespace WordCounter
       }
     }
 
-    public static void DisplayMessage()
+    public static void DisplayMatches()
     {
       if (!newRepeatCounter.ContainsWord())
       {
-        Console.WriteLine(">>> Sorry, no match was found. :(");
+        Console.WriteLine(">>> Sorry, there is absolutely none matching your sentence. :(");
       }
       else if (newRepeatCounter.ContainsWord())
       {
-        Console.WriteLine($">>> {newRepeatCounter.FindWord()} match(es) for {newRepeatCounter.SearchFor}");
+        Console.WriteLine($">>> {newRepeatCounter.FindWord()} match(es) found for {newRepeatCounter.SearchFor}");
+      }
+      else
+      {
+        Console.WriteLine(">>> Search failed. Please try again.");
+      }
+    }
+
+    public static void AskBackToSearch()
+    {
+      Console.WriteLine(">>> Would you like to use WORD COUNTER again? (Y/N)");
+      string answer = Console.ReadLine().ToLower();
+      if (answer == "y" || answer == "yes")
+      {
+        searchIsOver = false;
+      }
+      else if (answer == "n" || answer == "no")
+      {
+        searchIsOver = true;
+        Console.WriteLine(">>> Thanks for using WORD COUNTER. See you next time!");
       }
       else
       {
         Console.WriteLine(">>> Invalid input. Please try again.");
+        AskBackToSearch();
       }
     }
   }
