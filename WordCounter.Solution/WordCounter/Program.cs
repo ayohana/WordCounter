4d44ddd9e65fc3482407a7032b525a6222187ee6
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-06 11:13:48 -0800

    Add welcome message and UI logic

--- WordCounter.Solution/WordCounter/Program.cs
+++ WordCounter.Solution/WordCounter/Program.cs
@@ -1,5 +1,5 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using WordCounter.Models;
 
 namespace WordCounter
@@ -8,9 +8,32 @@ namespace WordCounter
   {
     public static void Main()
     {
+      Welcome();
+      Console.WriteLine("Enter a word you'd like to search for:");
+      string searchFor = Console.ReadLine();
+      Console.WriteLine("Enter a sentence:");
+      string sentence = Console.ReadLine();
+      RepeatCounter newRepeatCounter = new RepeatCounter(searchFor, sentence);
+      if (!newRepeatCounter.ContainsWord())
+      {
+        Console.WriteLine("Sorry, this sentence does not contain the word you are looking for.");
+      }
+      else if (newRepeatCounter.ContainsWord())
+      {
+        Console.WriteLine(newRepeatCounter.FindWord());
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please try again.");
+      }
+    }
 
-      // UI code goes here
-
+    public static void Welcome()
+    {
+      Console.WriteLine(Environment.NewLine + "  _      __            __  _____               __         ");
+      Console.WriteLine(" | | /| / /__  _______/ / / ___/__  __ _____  / /____ ____");
+      Console.WriteLine(" | |/ |/ / _ \\/ __/ _  / / /__/ _ \\/ // / _ \\/ __/ -_) __/");
+      Console.WriteLine(" |__/|__/\\___/_/  \\_,_/  \\___/\\___/\\_,_/_//_/\\__/\\__/_/   " + Environment.NewLine);
     }
   }
 }
