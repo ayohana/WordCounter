35429e83f1b4e472bc3812c4496d101ae81caa97
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-06 11:17:22 -0800

    Refactor UI logic

--- WordCounter.Solution/WordCounter/Program.cs
+++ WordCounter.Solution/WordCounter/Program.cs
@@ -6,14 +6,34 @@ namespace WordCounter
 {
   public class Program
   {
+    public static RepeatCounter newRepeatCounter { get; set; }
+
     public static void Main()
     {
       Welcome();
+      AskUser();
+      DisplayMessage();
+    }
+
+    public static void Welcome()
+    {
+      Console.WriteLine(Environment.NewLine + "  _      __            __  _____               __         ");
+      Console.WriteLine(" | | /| / /__  _______/ / / ___/__  __ _____  / /____ ____");
+      Console.WriteLine(" | |/ |/ / _ \\/ __/ _  / / /__/ _ \\/ // / _ \\/ __/ -_) __/");
+      Console.WriteLine(" |__/|__/\\___/_/  \\_,_/  \\___/\\___/\\_,_/_//_/\\__/\\__/_/   " + Environment.NewLine);
+    }
+
+    public static void AskUser()
+    {
       Console.WriteLine("Enter a word you'd like to search for:");
       string searchFor = Console.ReadLine();
       Console.WriteLine("Enter a sentence:");
       string sentence = Console.ReadLine();
-      RepeatCounter newRepeatCounter = new RepeatCounter(searchFor, sentence);
+      newRepeatCounter = new RepeatCounter(searchFor, sentence);
+    }
+
+    public static void DisplayMessage()
+    {
       if (!newRepeatCounter.ContainsWord())
       {
         Console.WriteLine("Sorry, this sentence does not contain the word you are looking for.");
@@ -26,14 +46,6 @@ namespace WordCounter
       {
         Console.WriteLine("Invalid input. Please try again.");
       }
-    }
-
-    public static void Welcome()
-    {
-      Console.WriteLine(Environment.NewLine + "  _      __            __  _____               __         ");
-      Console.WriteLine(" | | /| / /__  _______/ / / ___/__  __ _____  / /____ ____");
-      Console.WriteLine(" | |/ |/ / _ \\/ __/ _  / / /__/ _ \\/ // / _ \\/ __/ -_) __/");
-      Console.WriteLine(" |__/|__/\\___/_/  \\_,_/  \\___/\\___/\\_,_/_//_/\\__/\\__/_/   " + Environment.NewLine);
     }
   }
 }
